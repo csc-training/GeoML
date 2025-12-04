f61568aaf85fef03850d00be894fa3f72321bcd8
Iidahak <75577529+Iidahak@users.noreply.github.com>
2025-12-04 15:26:07 +0200

    Update 09_2_train_model.sh
    Fix number of cores in object detection model training

--- 09_object_detection/own_model_training/09_2_train_model.sh
+++ 09_object_detection/own_model_training/09_2_train_model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --account=project_462001167 # Choose the project to be billed. Change to own project, if used outside of the course
-#SBATCH --partition=small-g         # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job                
+#SBATCH --account=project_462001167 # Choose the project to be billed. Change to own project, if used outside of the course.
+#SBATCH --partition=small-g         # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job.             
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
-#SBATCH --cpus-per-task=8           # How many processors work on one task. Upper limit depends on number of CPUs per node.
-#SBATCH --gpus=1                    # Number of GPUs
+#SBATCH --cpus-per-task=4           # How many processors work on one task. Upper limit depends on number of CPUs per node. Here we choose 4 cores because the Ultralytics YOLO model can use a maximum of 8 cores, and in Lumi one physical core corresponds to two virtual cores.  
+#SBATCH --gpus=1                    # Number of GPUs.
 #SBATCH --time=00:30:00             # Maximum duration of the job. Upper limit depends on partition.
-#SBATCH --mem=32G                   # Reserved memory
+#SBATCH --mem=32G                   # Reserved memory.
 
 # Load the CSC module tree into use
 module use /appl/local/csc/modulefiles/
