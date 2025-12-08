08a4acb7f2bddb1c2a2f0e31dec288b73044d3cc
Kylli Ek <kylli.ek@csc.fi>
2025-12-08 13:30:28 +0200

    Update 08_1_train_model.sh

--- 08_cnn_segmentation/08_1_train_model.sh
+++ 08_cnn_segmentation/08_1_train_model.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account=project_462001167 # Choose the project to be billed. Change to own project, if used outside of the course
-#SBATCH --partition=small-g           # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --partition=small-g        # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 #SBATCH --ntasks=1                # Number of tasks. Upper limit depends on partition.
-#SBATCH --cpus-per-task=7       # How many processors work on one task. Upper limit depends on number of CPUs per GPU. In Lumi there are 7 CPU cores per one GPU. 
+#SBATCH --cpus-per-task=7         # How many processors work on one task. Upper limit depends on number of CPUs per GPU. In Lumi there are 7 CPU cores per one GPU. 
 #SBATCH --mem=60G                 # Reserved memory
 #SBATCH --time=00:30:00           # Maximum duration of the job. Upper limit depends on partition.
-#SBATCH --gpus=1         # Number of GPUs (LUMI version)
-# SBATCH --gres=gpu:v100:1         # Number of GPUs (Puhti version)
+#SBATCH --gpus=1                  # Number of GPUs (LUMI version)
+# SBATCH --gres=gpu:v100:1        # Number of GPUs (Puhti version)
 
 # Load the CSC module tree into use
 module use /appl/local/csc/modulefiles/
