cd94836b5e6061074bea2c1e6756a224fb9cf465
Kylli Ek <kylli.ek@csc.fi>
2025-12-08 12:17:09 +0200

    remove comments

--- 08_cnn_segmentation/08_1_train_model.sh
+++ 08_cnn_segmentation/08_1_train_model.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-# SBATCH --account=project_462001167 # Choose the project to be billed. Change to own project, if used outside of the course
-# SBATCH --partition=small-g           # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
-# SBATCH --ntasks=1                # Number of tasks. Upper limit depends on partition.
-# SBATCH --cpus-per-task=7       # How many processors work on one task. Upper limit depends on number of CPUs per node. In Lumi there are 7 CPU cores per one GPU. 
-# SBATCH --mem=60G                 # Reserved memory
-# SBATCH --time=02:00:00           # Maximum duration of the job. Upper limit depends on partition.
-# SBATCH --gpus-per-node=1         # Number of GPUs
-# SBATCH --gres=gpu:v100:1         # Number of GPUs (Puhti GPU allocation)
-# SBATCH --reservation geoml-gpu  # Reservation, only available during the course TODOS
+#SBATCH --account=project_462001167 # Choose the project to be billed. Change to own project, if used outside of the course
+#SBATCH --partition=small-g           # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
+#SBATCH --ntasks=1                # Number of tasks. Upper limit depends on partition.
+#SBATCH --cpus-per-task=7       # How many processors work on one task. Upper limit depends on number of CPUs per GPU. In Lumi there are 7 CPU cores per one GPU. 
+#SBATCH --mem=60G                 # Reserved memory
+#SBATCH --time=00:30:00           # Maximum duration of the job. Upper limit depends on partition.
+#SBATCH --gpus=1         # Number of GPUs (LUMI version)
+# SBATCH --gres=gpu:v100:1         # Number of GPUs (Puhti version)
 
 # Use course Python installation, inc TorchGeo
 #export PATH="/projappl/project_462001167/geoml_tykky2/bin:$PATH"
