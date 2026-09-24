b69edd84eff4840478f8ba4f349cb7541337d715
Katri Tegel <17436908+katrite@users.noreply.github.com>
2026-09-24 17:58:47 +0300

    Update SLURM account for job billing

--- 07_cnn_segmentation/07B_using_foundation_model/07B_2_predict.sh
+++ 07_cnn_segmentation/07B_using_foundation_model/07B_2_predict.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-#SBATCH --account=project_2000599   # Choose the project to be billed. Change to own project, if used outside of the course
+#SBATCH --account=project_2019932   # Choose the project to be billed. Change to own project, if used outside of the course
 #SBATCH --partition=gpumedium       # Which queue to use. Defines maximum time, memory, tasks, nodes and local storage for job
 #SBATCH --ntasks=1                  # Number of tasks. Upper limit depends on partition.
 #SBATCH --cpus-per-task=8           # How many processors work on one task. Upper limit depends on number of CPUs per GPU. In LUMI there are 7 CPU cores per one GPU. 
